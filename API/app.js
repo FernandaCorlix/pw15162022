b11969e9a4488a0c6a33232722a1666c608b736e
Fernanda Corlix <19171347@itculiacan.edu.mx>
2022-11-16 15:02:52 -0700

    API

---
+++ API/app.js
@@ -0,0 +1,15 @@
+let express=requiere('express')
+let mysql=requiere('mysql')
+let app = express()
+let puerto = 3000
+
+app.listen(puerto, function(){
+    console.log("Servidor en linea")
+})
+
+
+
+
+
+
+
