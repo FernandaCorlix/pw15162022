b188be21db4d88049bc61845e504b81a6be39abc
Fernanda Corlix <19171347@itculiacan.edu.mx>
2022-11-27 10:49:45 -0700

    VUE

--- API/app.js
+++ API/app.js
@@ -1,15 +1,64 @@
-let express=requiere('express')
-let mysql=requiere('mysql')
-let app = express()
-let puerto = 3000
+let express =require('express');
+let mysql=require('mysql');
+let app = express();
+let puerto = 3000;
 
 app.listen(puerto, function(){
-    console.log("Servidor en linea")
+    console.log("Servidor en linea");
+});
+//Base de datos
+//Parametros de conexion 
+
+
+//Los objetos van entre llaves
+let conexion=mysql.createConnection({
+    host: 'localhost',
+    user: 'pw', //'root/
+    password: '',
+    database: 'pw'
 })
 
+conexion.connect(function(error){
+    if(error){
+        throw error
+    }else{
+        console.log('Conectado a la base de datos')
+    }
+})
 
+//Que obtenga cosas a traves de rutas
+//Ruta de inicio  que es la raiz 
+app.get('/', function(req,res){
+    res.send("Ruta de inicio");
+})
 
+//Ruta a un articulos 
+app.get('/api/articulos/:id',function(req,res){
+    conexion.query("SELECT * FROM articulos WHERE id = ?", [req.params.id], function(error,fila){
+        if(error){
+            throw error
+        }else{
+            res.send(fila);
+        }
+    })
+})
 
+//Ruta para agregar un articulo
+app.post('/api/articulos',function(req,res){
+    let data = {descripcion:req.body.descripcion,
+                precio:req.body.precio,
+            cantidad:req.body.cantidad}
 
+            let sql = "INSERT INTO articulos SET ? "
+            conexion.query(sql,data,function(error,results){
+                if(error){
+                    throw error
+                }else{
+                    res.send(results);
+                } 
+            })
+    })
+
+//Para modificar un registro ya existe
 
 
---
+++ Servidor/app.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var mysql = requiere('mysql')
+
+var app = express()
+
+app.listen("3000", function(){
+    console.log('Servidor en linea... OK')
+})
+
