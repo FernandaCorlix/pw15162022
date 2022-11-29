99b2ff961cb5853841665d395f8f4705fdb9490a
Fernanda Corlix <19171347@itculiacan.edu.mx>
2022-11-29 12:56:02 -0700

    API

--- API/app.js
+++ API/app.js
@@ -1,64 +1,91 @@
-let express =require('express');
-let mysql=require('mysql');
+//Variables de ambiente
+let express = require('express');
+let mysql = require('mysql');
 let app = express();
+let cors = require('cors');
+app.use(express.json());
+//Pa que sepa
 let puerto = 3000;
-
-app.listen(puerto, function(){
-    console.log("Servidor en linea");
+//Donde los escucha
+app.listen(puerto, () => {
+    console.log('Servicio iniciado');
 });
-//Base de datos
-//Parametros de conexion 
 
-
-//Los objetos van entre llaves
-let conexion=mysql.createConnection({
+//Para que se cree la conexion 
+let conexion = mysql.createConnection({
     host: 'localhost',
-    user: 'pw', //'root/
+    user: 'pw',
     password: '',
     database: 'pw'
-})
+});
 
+//Para que lo se conecte la conexcion xd
 conexion.connect(function(error){
     if(error){
-        throw error
+        throw error;
     }else{
-        console.log('Conectado a la base de datos')
+        console.log("Conectado a la base de datos");
     }
 })
 
-//Que obtenga cosas a traves de rutas
-//Ruta de inicio  que es la raiz 
-app.get('/', function(req,res){
-    res.send("Ruta de inicio");
-})
-
-//Ruta a un articulos 
-app.get('/api/articulos/:id',function(req,res){
-    conexion.query("SELECT * FROM articulos WHERE id = ?", [req.params.id], function(error,fila){
-        if(error){
-            throw error
-        }else{
-            res.send(fila);
+app.get('/api/articulos', function (req, res) {
+    conexion.query('SELECT * FROM Articulos', [], function (error, results) {
+        if (error) {
+            throw error;
         }
-    })
-})
+        else {
+            res.status(200).send(results);
+        }
+    });
+});
 
-//Ruta para agregar un articulo
-app.post('/api/articulos',function(req,res){
-    let data = {descripcion:req.body.descripcion,
-                precio:req.body.precio,
-            cantidad:req.body.cantidad}
+//Con el post se inserta un objeto nuevo 
+app.post('/api/articulos', function (req, res) {
+    const data = {
+        descripcion: req.body.descripcion,
+        precio: req.body.precio,
+        cantidad: req.body.cantidad,
+    };
 
-            let sql = "INSERT INTO articulos SET ? "
-            conexion.query(sql,data,function(error,results){
-                if(error){
-                    throw error
-                }else{
-                    res.send(results);
-                } 
-            })
-    })
+    const sql = 'INSERT INTO Articulos SET ?';
 
-//Para modificar un registro ya existe
+    //Para mandarlo a ejecutar 
+    conexion.query(sql, data, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.status(200).send(results);
+        }
+    });
+});
 
+//Put se usa para actualizar datos
+app.put('/api/articulos/:id', function (req, res) {
+    const { id } = req.params;
+    const { descripcion, precio, cantidad } = req.body;
+    const sql = 'UPDATE Articulos SET descripcion = ?, precio = ?, cantidad = ? WHERE id = ?';
 
+    conexion.query(sql, [descripcion, precio, cantidad, id], function (error, results) {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.status(200).send(results);
+        }
+    });
+});
+
+//Para borrar objetos
+app.delete('/api/articulos/:id', function (req, res) {
+    const { id } = req.params;
+
+    conexion.query('DELETE FROM Articulos WHERE id = ?', [id], function (error, results) {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.status(200).send(results);
+        }
+    });
+});
